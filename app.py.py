d189daa5e0357873ecfbf8c5a0a9f957c013fa04
maitrewalid25-collab <maitrewalid25@gmail.com>
2026-05-02 17:38:52 +0100

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -49,7 +49,7 @@ st.markdown("""
 st.markdown("""
     <div class="hero-section">
         <h1>🛡️ Marka-Sentry: نظام الرصد الميداني</h1>
-        <p style="font-size: 1.2em;">دمج الذكاء الاصطناعي البصري مع الخبرة القانونية الجزائرية</p>
+        <p style="font-size: 1.2em;">دمج السرعة الميدانية مع الخبرة القانونية الجزائرية</p>
     </div>
     """, unsafe_allow_html=True)
 
